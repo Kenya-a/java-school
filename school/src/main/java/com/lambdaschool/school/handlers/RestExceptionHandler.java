9b1dec62d7f64228c9212197b02c9a8ab92125bc
Kenya-a <Kenya-a@users.noreply.github.com>
2019-09-16 17:37:22 -0500

    exception handler (response to be fixed)

--- school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -1,10 +1,41 @@
 package com.lambdaschool.school.handlers;
 
+import com.lambdaschool.school.exceptions.ResourceNotFoundException;
+import com.lambdaschool.school.model.ErrorDetail;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 //bean shared by the controller
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler
 {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException rnfe, HttpServletRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDevMessage(rnfe.getClass().getName());
+
+        return new ResponseEntity<>(errorDetail,null,HttpStatus.NOT_FOUND);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        return super.handleNoHandlerFoundException(ex, headers, status, request);
+    }
 }
